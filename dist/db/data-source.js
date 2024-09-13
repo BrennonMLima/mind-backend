3ca23b330075a92d3b22856602284a3f2c53d208
BrennonLLima <brennon.lima@fatec.sp.gov.br>
2024-09-13 14:53:03 -0300

    ajustes

--- dist/db/data-source.js
+++ dist/db/data-source.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "BrennonLima",
-    password: "Brennon_123",
-    database: "mind",
-    entities: ["src/models/*.ts"],
-    logging: true,
-    synchronize: true,
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "",
+  password: "",
+  database: "mind",
+  entities: ["src/models/*.ts"],
+  logging: true,
+  synchronize: true,
 });
